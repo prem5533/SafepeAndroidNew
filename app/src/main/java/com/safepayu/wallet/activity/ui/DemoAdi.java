060ccff2407861c38491be5ed7ce8369ee951965
Naga Raj <nagarazbuganatti@gmail.com>
2020-01-02 10:56:32 +0530

    1 jan,2020 morning after pull

--- app/src/main/java/com/safepayu/wallet/activity/ui/DemoAdi.java
+++ app/src/main/java/com/safepayu/wallet/activity/ui/DemoAdi.java
@@ -3,7 +3,6 @@ package com.safepayu.wallet.activity.ui;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.multidex.MultiDex;
@@ -24,6 +23,5 @@ public class DemoAdi extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.navigation_new);
-        Toast.makeText(DemoAdi.this, "hello", Toast.LENGTH_SHORT).show();
     }
 }
